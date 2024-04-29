6c78762d4c37e374464934cdd25ca003dd439815
chaptrhouse <chaptrhouse@gmail.com>
2024-04-29 00:05:34 -0500

    🧹 chore: clean up console logs

--- app/api/stargazers/route.ts
+++ app/api/stargazers/route.ts
@@ -7,7 +7,6 @@ export const revalidate = 60 * 60; // 1 hour
 export async function GET() {
   const repoUrl = process.env.NEXT_PUBLIC_REPO_URL!;
   const apiUrl = `https://api.github.com/repos/${repoUrl.split("github.com/")[1]}`;
-  console.log(apiUrl);
 
   try {
     const response = await axios.get(apiUrl, {
